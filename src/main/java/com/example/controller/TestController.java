d7bd505384bf4df94df94d395963c3e5f702d6f3
wangjiameng <123@qq.com>
2020-08-18 16:49:44 +0800

    init project

---
+++ src/main/java/com/example/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+/**
+ * @author wangjiameng
+ * @date 2020-08-18 13:59
+ */
+public class TestController {
+
+    String s;
+
+    public String say(){
+        return "s";
+    }
+}
